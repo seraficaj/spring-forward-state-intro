2f8727cb9538c8e5e3c818e50b7ceb136d1abd1e
Jason Serafica <seraficaj@gmail.com>
2022-03-09 11:29:04 -0800

    add reset mood button

--- src/MoodTracker.jsx
+++ src/MoodTracker.jsx
@@ -1,24 +1,48 @@
-import React, { Component } from 'react';
-import MoodPoints from './MoodPoints';
+import React, { Component } from "react";
+import { Button, ButtonGroup } from "react-bootstrap";
+import MoodPoints from "./MoodPoints";
 
 class MoodTracker extends Component {
-    // using a contructor to manage class
-    constructor() {
-        // invoke super
-        super()
-        // define state obj
-        this.state = { points: 9}
-        // run any init you need
-    }
-    state = {  } 
-    render() { 
+    // using class field decoration
+    state = {
+        points: 9,
+    };
+
+    handleIncreaseMood = () => {
+        // setstate triggers a re-render
+        // takes a cb or obj of new state
+        // if you need to refer to prev state, you have to use the cb function
+        this.setState((prevState, props) => {
+            // whatever is returned -- will be the new state
+            // previous state is incremented
+            return {
+                points: prevState.points + 1, // increment prevState by 1
+            };
+        });
+    };
+
+    handleResetTen = () => {
+        this.setState({
+            points: 10,
+        });
+    };
+
+    render() {
         return (
             <div>
-                <h1 className='text-center'>Mood Tracker</h1>
-                <MoodPoints points = {this.state.points} />
+                <h1 className="text-center">Mood Tracker</h1>
+                <ButtonGroup>
+                    <Button onClick={this.handleIncreaseMood} variant="primary">
+                        🌈
+                    </Button>
+                    <Button onClick={this.handleResetTen} variant="secondary">
+                        Reset Mood
+                    </Button>
+                </ButtonGroup>
+                <MoodPoints points={this.state.points} />
             </div>
         );
     }
 }
- 
+
 export default MoodTracker;
